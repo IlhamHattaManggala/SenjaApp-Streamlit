8b2533ae4be976ec242c54660d8a190d9c7c3cab
IlhamHattaManggala <122850473+IlhamHattaManggala@users.noreply.github.com>
2025-05-22 17:18:31 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 def get_db():
-    client = MongoClient("mongodb://localhost:27017")
+    client = MongoClient("mongodb+srv://capstonesmt6:capstonesmt6.25@senja-app.zut9mmt.mongodb.net/?retryWrites=true&w=majority&appName=Senja-App")
     db = client["senja"]
     return db
 
