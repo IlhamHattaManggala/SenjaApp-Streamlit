b274fce9dfb5af36ff4bea4819323ead0cf1d545
IlhamHattaManggala <122850473+IlhamHattaManggala@users.noreply.github.com>
2025-05-22 20:29:43 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,13 +3,12 @@ from pymongo.server_api import ServerApi
 
 def get_db():
     uri = "mongodb+srv://ilhamhattamanggala123:IlhamHatta311202@bigdata.yymouit.mongodb.net/?appName=BigData"
-    # Create a new client and connect to the server
     client = MongoClient(uri, server_api=ServerApi('1'))
-    # Send a ping to confirm a successful connection
     try:
         client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
+        print("Failed to connect to MongoDB:", e)
     db = client["senja"]
     return db
 
